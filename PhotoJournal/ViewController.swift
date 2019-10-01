1d5cdbd333ed098b407087d505ba482555cca7d3
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-10-01 15:31:05 -0400

    Initial commit

---
+++ PhotoJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoJournal
+//
+//  Created by Angela Garrovillas on 10/1/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
