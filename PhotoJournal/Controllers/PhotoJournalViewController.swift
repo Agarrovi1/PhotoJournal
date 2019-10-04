c972b4b1af6bd5e614af9f9942f06b0c65a17cd7
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-10-04 14:17:59 -0400

    makes view controller files

---
+++ PhotoJournal/Controllers/PhotoJournalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoJournalViewController.swift
+//  PhotoJournal
+//
+//  Created by Angela Garrovillas on 10/1/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import UIKit
+
+class PhotoJournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
